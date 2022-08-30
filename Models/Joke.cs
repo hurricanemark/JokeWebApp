51116bc574b321fa3243be8d7af5cea5b3be7119
markn <markn@aesclever.com>
2022-08-30 09:50:54 -0700

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        /* kbrd-shortcut: prop+tab+tab */
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        /* kbrd-shortcut: ctor+tab+tab */
+        public Joke()
+        {
+
+        }
+    }
+}
